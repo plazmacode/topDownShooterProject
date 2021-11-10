691400b0b3fa1d3f00230f0cc57f51fe705e61e4
Mads-hash <56913739+Mads-hash@users.noreply.github.com>
2021-11-10 11:19:37 +0100

    Update Enemy.cs

--- Classes/Enemy.cs
+++ Classes/Enemy.cs
@@ -6,5 +6,22 @@ namespace topDownShooterProject.Classes
 {
     class Enemy : Character
     {
+
+        /*private Vector2 targetPosition;
+
+        public override void Update(GameTime gameTime)
+        {
+            targetPosition = GameWorld.PlayerPosition;
+
+
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>("Mango");
+        }
+
+
+    */
+
     }
 }
