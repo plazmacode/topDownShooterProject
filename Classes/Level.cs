9d1ea1e05dc1374b2ebf98cf5161e074a8cf3f9f
plazmacode <93761763+plazmacode@users.noreply.github.com>
2021-11-17 13:22:39 +0100

    Update Level.cs

--- Classes/Level.cs
+++ Classes/Level.cs
@@ -294,14 +294,14 @@ namespace topDownShooterProject.Classes
 
         public static void Update(GameTime gameTime)
         {
-            playerLevelChange();
+            PlayerLevelChange();
         }
 
         /// <summary>
         /// Check if all enemies are killed
         /// Afterwards, check if the player is moving to another level in the levelArray
         /// </summary>
-        private static void playerLevelChange()
+        private static void PlayerLevelChange()
         {
             if (GameWorld.EnemiesLeft <= 0)
             {
